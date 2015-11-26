7467dcea8f47cb08112bc3bd515116eb12e16868
WangSwod <wangtu374@gmail.com>
2015-11-26 16:02:11 +0800

    update Demo for dialog, add code for sharedPreference

--- Demo for dialog/DialogDemo/app/src/main/java/com/example/apple/dialogdemo/MainActivity.java
+++ Demo for dialog/DialogDemo/app/src/main/java/com/example/apple/dialogdemo/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.apple.dialogdemo;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.InputType;
 import android.text.format.Time;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -21,10 +21,19 @@ public class MainActivity extends ActionBarActivity {
 
     static final String TAG= "MainActivity";
 
-    private  EditText date_Input,time_Input;
+    static  final String KEY_DATE ="key_date";
+    static  final String KEY_TIME = "key_time";
+    static  final String KEY_WEIGHT = "key_weight";
+    static  final String PREFERENCE = "MainActivity_Preference";
+
+    private  EditText date_Input,time_Input,weight_inpt;
 
     private DatePickerDialog date_Picker ;
     private TimePickerDialog time_Picker;
+
+    private  int year,month, day,hour,minute;
+
+
 
 
 
@@ -35,13 +44,11 @@ public class MainActivity extends ActionBarActivity {
 
         date_Input = (EditText)findViewById(R.id.date_Input);
         time_Input = (EditText)findViewById(R.id.time_Input);
-
-     //   time_Input.setInputType(InputType.TYPE_NULL);
-       // date_Input.setInputType(InputType.TYPE_NULL);
-
-
+        weight_inpt = (EditText)findViewById(R.id.weight_input);
+        //刷新为当前时间
+        refreshTime();
+        //初始化dialogs
         initDialog();
-
 
 
         date_Input.setOnTouchListener(new View.OnTouchListener() {
@@ -69,42 +76,43 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
-//        date_Input.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override
-//            public void onFocusChange(View v, boolean hasFocus) {
-//                if(hasFocus){
-//                    date_Input.setInputType(InputType.TYPE_NULL);
-//                    date_Picker.show();
-//                }
-//            }
-//        });
-//
-//        time_Input.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override
-//            public void onFocusChange(View v, boolean hasFocus) {
-//                if(hasFocus){
-//                    time_Input.setInputType(InputType.TYPE_NULL);
-//                    time_Picker.show();
-//                }
-//            }
-//        });
 
+        restoreUI();
+
+    }
+
+
+
+    private  void restoreUI(){
+        SharedPreferences restore_Preference = getSharedPreferences(PREFERENCE,Context.MODE_PRIVATE);
+
+        String restore_Date = restore_Preference.getString(KEY_DATE,year+"/"+month +"/"+day);
+        String restore_Time = restore_Preference.getString(KEY_TIME,hour+":"+minute);
+        String restore_Weight = restore_Preference.getString(KEY_WEIGHT,"0");
+
+        weight_inpt.setText(restore_Weight);
+        time_Input.setText(restore_Time);
+        date_Input.setText(restore_Date);
+
+    }
+
+  public void refreshTime(){
+        //获取当前date 和 time
+        Time time = new Time();
+        time.setToNow();
+        year = time.year;
+        month = time.month;
+        day = time.monthDay;
+        hour = time.hour;
+        minute = time.minute;
 
     }
 
     private  void initDialog(){
 
-        Time time = new Time();
-        time.setToNow();
-        int year = time.year;
-        int month = time.month;
-        int day = time.monthDay;
-        int hour = time.hour;
-        int minute = time.minute;
 
 
-        Log.i(TAG,year+"/"+month+"/"+day + " "+hour +":"+minute);
-
+        //实例化date_picker dialog
         DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -114,6 +122,8 @@ public class MainActivity extends ActionBarActivity {
 
         date_Picker = new DatePickerDialog(MainActivity.this,dateSetListener, year,month,day);
 
+
+        //实例化time_picker dialog
         TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -125,6 +135,24 @@ public class MainActivity extends ActionBarActivity {
         time_Picker = new TimePickerDialog(MainActivity.this,timeSetListener,hour,minute,true);
     }
 
+
+    @Override
+    protected void onPause() {
+
+        SharedPreferences save_Preference = getSharedPreferences(PREFERENCE, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = save_Preference.edit();
+
+        editor.putString(KEY_DATE,date_Input.getText().toString());
+        editor.putString(KEY_TIME,time_Input.getText().toString());
+        editor.putString(KEY_WEIGHT, weight_inpt.getText().toString());
+
+        editor.commit();
+
+        super.onPause();
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ Demo for dialog/DialogDemo/app/src/main/java/com/example/apple/dialogdemo/Util.java
@@ -0,0 +1,12 @@
+package com.example.apple.dialogdemo;
+
+import android.text.format.Time;
+
+/**
+ * Created by apple on 11/26/15.
+ */
+public class Util {
+
+
+
+}
